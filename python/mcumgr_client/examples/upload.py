3c27bc03129f125543e488f013fd60361fd1a5e5
Adrien Bruant <adrien.bruant@aalberts-hfc.com>
2024-07-03 18:02:54 +0200

    Add a python wrapper to mcumgr-client
    This commit adds a python module invoking mcumgr-client binary and the
    CI configuration to build wheels in various environment.
    
    To improve portability of wheels, default-features are disabled for
    serialport as it removes the dependency on libudev. There are no
    drawbacks that I know of as libudev is used to enumerate serial ports,
    which mcumgr-client does not use.

---
+++ python/mcumgr_client/examples/upload.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import mcumgr_client as mcgr
+import sys
+
+try:
+    s = mcgr.Session(sys.argv[1], 576000)
+    d = s.list()
+    print(d)
+
+    s.upload(sys.argv[2])
+
+    d = s.list()
+    print(d)
+
+    s.reset()
+except mcgr.CalledProcessError as e:
+    print(e)
+    print("--- STDERR [")
+    print(e.stderr)
+    print("] STDERR ---")
+
